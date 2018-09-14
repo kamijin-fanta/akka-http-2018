4dc8e6ba5e35606a32a0736a1a8404fbdd8e7180
kamijin_fanta <kamijin@live.jp>
2018-09-14 13:42:34 +0900

    directives

--- src/main/scala/Directives.scala
+++ src/main/scala/Directives.scala
@@ -8,17 +8,33 @@ object DirectivesCompose extends HttpApp {
     startServer("localhost", 8080)
   }
 
+  val getOrPost: Directive0 = get | post
+  val getUser: Directive1[String] = get & pathPrefix("user" / Segment)
+
   override def routes: Route =
     pathPrefix("foo") {
-      get {
-        complete("get foo content")
-      } ~ post {
-        complete("post foo content")
+      getOrPost {
+        complete("foo content")
       }
-    } ~ (get & extractUri) { uri =>
-      complete(s"hoge request ${uri.path}")
+    } ~ getUser { userName =>
+      complete(s"get user request $userName")
     } ~ {
       complete("other method request")
+    }
+}
+
+
+object DirectivesMap extends HttpApp {
+  def main(args: Array[String]): Unit = {
+    startServer("localhost", 8080)
+  }
+
+  val getUser: Directive1[String] = get & pathPrefix("user" / Segment)
+  val getUserUpperCase: Directive1[String] = getUser.map(_.toUpperCase)
+
+  override def routes: Route =
+    getUserUpperCase { userName =>
+      complete(s"UpperCase: $userName")
     }
 }
 
@@ -69,7 +85,7 @@ object DirectivesInternal4 extends HttpApp {
   def main(args: Array[String]): Unit = {
     startServer("localhost", 8080)
   }
-
+val hoge: Directive1[String] = get & pathPrefix("user" / Segment)
   override def routes: Route =
     customMethodExtractor { method =>
       complete(s"bar content $method")
